dcc02391229712394004a239557dcd5c32771235
tillfall@126.com <tillfall@126.com>
2017-08-08 21:55:46 +0800

    init

---
+++ ddns.sh
@@ -0,0 +1,2 @@
+/usr/local/bin/lynx -mime_header -auth=tillfall:tillfall "http://members.3322.net/dyndns/update?system=dyndns&hostname=tillfall.f3322.net" > /home/tf/ddns.log
+date >> /home/tf/ddns.log

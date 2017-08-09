d464bf8e8298e019ccc144f3da941778395930f0
tillfall <tillfall@126.com>
2017-08-09 19:24:52 +0800

    Increase Net 10%

--- fund/myfund.py
+++ fund/myfund.py
@@ -2,9 +2,10 @@ from urllib.request import urlopen
 from myconst import HTMLTABLE
 
 class Fund:
-    def __init__(self, fundNo, buyPrice=0, buyCount=0, isRMB="", buyDate=''):
+    def __init__(self, fundNo, buyPrice=0, newBasePrice = 0, buyCount=0, isRMB="", buyDate=''):
         self.fundNo = fundNo
         self.buyPrice = buyPrice
+        self.newBasePrice = newBasePrice
         self.buyCount = buyCount
         self.isRMB = isRMB
         self.buyDate = buyDate
@@ -38,32 +39,37 @@ class Fund:
         self.nowPrice = value[1]
         
     def newPrice2Tr(self):
+        newPriceFloat = float(self.nowPrice)
         ret = '<tr>\n'
         ret += '<td>' + self.name6 + '</td>\n'
-        ret += '<td>' + '%.2f' % ((float(self.nowPrice)-float(self.buyPrice))/float(self.buyPrice)*100) +'%' + '</td>\n'
+        ret += '<td>' + '%.2f' % ((newPriceFloat-self.buyPrice)/self.buyPrice*100) +'%' + '</td>\n'
+        if self.newBasePrice == 0:
+            ret += '<td></td>\n'
+        else:
+            ret += '<td>' + '%.2f' % ((newPriceFloat-self.buyPrice*self.newBasePrice)/(self.buyPrice*self.newBasePrice)*100) +'%' + '</td>\n'
         ret += '<td>' + self.nowDate + '</td>\n'
-        ret += '<td>' + self.isRMB + '%.2f' % ((float(self.nowPrice)-float(self.buyPrice))*float(self.buyCount)) + '</td>\n'
-        ret += '<td>' + self.nowPrice + '</td>\n'
-        ret += '<td>' + self.buyPrice + '</td>\n'
-        ret += '<td>' + self.buyCount + '</td>\n'
+        ret += '<td>' + self.isRMB + '%.2f' % ((newPriceFloat-self.buyPrice)*self.buyCount) + '</td>\n'
+        ret += '<td>' + str(self.nowPrice) + '</td>\n'
+        ret += '<td>' + str(self.buyPrice) + '</td>\n'
+        ret += '<td>' + str(self.buyCount) + '</td>\n'
         ret += '<td>' + self.buyDate + '</td>\n'
         ret += '</tr>\n'
         return ret
         
 def getmyfund():
     funds = []
-    funds.append(Fund('002402', '0.162', '30867', '$', '16-10-18'))
-    funds.append(Fund('003721', '0.1614', '278731', '$', '17-05-12'))
-    funds.append(Fund('164906', '1.2432', '329790', '', '17-05-22'))
+    funds.append(Fund('002402', 0.162, 0, 30867, '$', '16-10-18'))
+    funds.append(Fund('003721', 0.1614, 0, 278731, '$', '17-05-12'))
+    funds.append(Fund('164906', 1.2432, 1.1, 329790, '', '17-05-22'))
     for f in funds:
         f.getNewPrice()
         
     ret = '<html><title>fund</title>%s\n' % HTMLTABLE
-    ret += '<tr><td>name</td><td>%</td><td>date</td><td>$</td><td>Now$</td><td>Buy$</td><td>count</td><td>start</td></tr>'
+    ret += '<tr><td>name</td><td>%</td><td>%%</td><td>date</td><td>$</td><td>Now$</td><td>Buy$</td><td>count</td><td>start</td></tr>'
     for f in funds:
         ret += f.newPrice2Tr()
     ret += '</table>\n'
-    ret += 'ver:170804-1'
+    ret += 'ver:170809-1'
     ret += '</html>'
     return ret
         

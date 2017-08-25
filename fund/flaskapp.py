c865bced8d9abfa38259cb5fc1b78b8b1569d513
tillfall <tillfall@126.com>
2017-08-25 20:41:44 +0800

    refresh and down eastmoney

--- fund/flaskapp.py
+++ fund/flaskapp.py
@@ -1,39 +1,28 @@
 from flask import Flask, request
 from myfund import getmyfund
+from top_of_eastmoney import getfileslink, save
+from urls import load, visit, filter, rmv, ROOTURL, mod, domod, add, doadd
         
 app = Flask(__name__)
 
+@app.route('/<filename>')
+def downfile(filename):
+    return None
+    
 @app.route('/myfund')
 def fund():
     return getmyfund()
     
-@app.route('/')
-def hello_world():
-    return "hello tillfall"
+@app.route('/downeastmoney')
+def down():
+    return getfileslink()
     
-@app.route('/buy')
-def buy():
-    f = open('fund_buy.htm', encoding='gbk')
-    htm = f.read()
-    f.close()
-    return htm
-    
-@app.route('/doBuy', methods=['GET', 'POST'])
-def dobuy():
-    if request.method == 'POST':
-        return request.form['fundid']
-    else:
-        return request.args['fundid']
-        
-@app.route('/test')
-def test():
-    return '<img src="%s"/>' % 'http://api.map.baidu.com/staticimage/v2?ak=601263224020a7291f88e9dcc94c6810&width=400&height=600&zoom=8&center=86.814,47.429&labels=85.74,46.14|86.873,47.707|86.790,48.699|86.709,48.530|87.439,48.574|86.681,47.846&labelStyles=A%E4%B8%96%E7%95%8C%E9%AD%94%E9%AC%BC%E5%9F%8E,1,14,0xffffff,0x000fff,1|B%E5%B8%83%E5%B0%94%E6%B4%A5,1,14,0xffffff,0x000fff,1|C%E5%96%80%E7%BA%B3%E6%96%AF,1,14,0xffffff,0x000fff,1|D%E7%99%BD%E5%93%88%E5%B7%B4%E6%9D%91,1,14,0xffffff,0x000fff,1|E%E7%A6%BE%E6%9C%A8,1,14,0xffffff,0x000fff,1|F%E4%BA%94%E5%BD%A9%E6%BB%A9,1,14,0xffffff,0x000fff,1'
-    
-    
+@app.route('/geteastmoney')
+def getfromeastmoney():
+    return save()    
     
 ########  URLS
 
-from urls import load, visit, filter, rmv, ROOTURL, mod, domod, add, doadd
 @app.route(ROOTURL)
 def urlapp():
     allargs = request.args
@@ -71,5 +60,29 @@ def map1710():
     f.close()
     return htm
 
+'''
+@app.route('/')
+def hello_world():
+    return "hello tillfall"
+    
+@app.route('/buy')
+def buy():
+    f = open('fund_buy.htm', encoding='gbk')
+    htm = f.read()
+    f.close()
+    return htm
+    
+@app.route('/doBuy', methods=['GET', 'POST'])
+def dobuy():
+    if request.method == 'POST':
+        return request.form['fundid']
+    else:
+        return request.args['fundid']
+        
+@app.route('/test')
+def test():
+    return '<img src="%s"/>' % 'http://api.map.baidu.com/staticimage/v2?ak=601263224020a7291f88e9dcc94c6810&width=400&height=600&zoom=8&center=86.814,47.429&labels=85.74,46.14|86.873,47.707|86.790,48.699|86.709,48.530|87.439,48.574|86.681,47.846&labelStyles=A%E4%B8%96%E7%95%8C%E9%AD%94%E9%AC%BC%E5%9F%8E,1,14,0xffffff,0x000fff,1|B%E5%B8%83%E5%B0%94%E6%B4%A5,1,14,0xffffff,0x000fff,1|C%E5%96%80%E7%BA%B3%E6%96%AF,1,14,0xffffff,0x000fff,1|D%E7%99%BD%E5%93%88%E5%B7%B4%E6%9D%91,1,14,0xffffff,0x000fff,1|E%E7%A6%BE%E6%9C%A8,1,14,0xffffff,0x000fff,1|F%E4%BA%94%E5%BD%A9%E6%BB%A9,1,14,0xffffff,0x000fff,1'
+''' 
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0')# listen all ip address
--- fund/top_of_eastmoney.py
+++ fund/top_of_eastmoney.py
@@ -2,36 +2,49 @@ from selenium import webdriver
 import pandas as pd
 import time
 from datetime import datetime
+from os import listdir
+
+PATH = 'static/top_of_eastmoney/'
 
 def getPct(str):
     try:
         return float(str[:-1])
     except:
         return 0
+
+def save():        
+    drv = webdriver.PhantomJS()
+    drv.get('http://fund.eastmoney.com/data/fundranking.html#tall;c0;r;szzf;pn50;ddesc;qsd20160821;qed20170821;qdii;zq;gg;gzbd;gzfs;bbzt;sfbb')
+    tbl = drv.find_element_by_id('dbtable')
+    trs = tbl.find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
+
+    data = pd.DataFrame(columns=["1",
+                         "sn","id","name","date","net","all-net",
+                         "%","%7","%30","%90","%180","%360","%720",
+                        "%3y","%y","%all","%u",
+                        "fee","2"])
+                        
+    for tr in trs:
+        tds = tr.find_elements_by_tag_name('td')
+        rec = {"1":tds[0].text, "sn":tds[1].text, "id":tds[2].text, "name":tds[3].text, "date":tds[4].text, "net":float(tds[5].text), "all-net":float(tds[6].text),
+              "%":getPct(tds[7].text),"%7":getPct(tds[8].text),"%30":getPct(tds[9].text),"%90":getPct(tds[10].text),
+               "%180":getPct(tds[11].text),"%360":getPct(tds[12].text),"%720":getPct(tds[13].text),
+              "%3y":getPct(tds[14].text),"%y":getPct(tds[15].text),"%all":getPct(tds[16].text),"%u":getPct(tds[17].text),
+              "fee":getPct(tds[18].text),"2":tds[19].text,}
+        data = data.append(rec,ignore_index=True)
         
-drv = webdriver.PhantomJS()
-drv.get('http://fund.eastmoney.com/data/fundranking.html#tall;c0;r;szzf;pn50;ddesc;qsd20160821;qed20170821;qdii;zq;gg;gzbd;gzfs;bbzt;sfbb')
-tbl = drv.find_element_by_id('dbtable')
-trs = tbl.find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
+    data['%sort'] = data.apply(lambda x: (x['%7']*96*3 + x['%30']*24*5 + x['%90']*8*2 + x['%180']*4 + x['%360']*2 + x['%720'])/100, axis=1)
+    data = data.sort_values(by='%sort', ascending=False)
 
-data = pd.DataFrame(columns=["1",
-                     "sn","id","name","date","net","all-net",
-                     "%","%7","%30","%90","%180","%360","%720",
-                    "%3y","%y","%all","%u",
-                    "fee","2"])
-                    
-for tr in trs:
-    tds = tr.find_elements_by_tag_name('td')
-    rec = {"1":tds[0].text, "sn":tds[1].text, "id":tds[2].text, "name":tds[3].text, "date":tds[4].text, "net":float(tds[5].text), "all-net":float(tds[6].text),
-          "%":getPct(tds[7].text),"%7":getPct(tds[8].text),"%30":getPct(tds[9].text),"%90":getPct(tds[10].text),
-           "%180":getPct(tds[11].text),"%360":getPct(tds[12].text),"%720":getPct(tds[13].text),
-          "%3y":getPct(tds[14].text),"%y":getPct(tds[15].text),"%all":getPct(tds[16].text),"%u":getPct(tds[17].text),
-          "fee":getPct(tds[18].text),"2":tds[19].text,}
-    data = data.append(rec,ignore_index=True)
+    fn = PATH + 'funds'+datetime.strftime(datetime.now(), '%y-%m-%d')+'.csv'
+    data.to_csv(fn,encoding='utf_8_sig')
+
+    drv.quit()
+    return '<a href="' + fn + '">down</a>'
     
-data['%sort'] = data.apply(lambda x: (x['%7']*96*3 + x['%30']*24*5 + x['%90']*8*2 + x['%180']*4 + x['%360']*2 + x['%720'])/100, axis=1)
-data = data.sort_values(by='%sort', ascending=False)
-
-data.to_csv('top_of_eastmoney/funds'+datetime.strftime(datetime.now(), '%y-%m-%d')+'.csv',encoding='utf_8_sig')
-
-drv.quit()
+def getfileslink():
+    files = listdir(PATH)
+    ret = []
+    for file in files:
+        ret.append('<li><a href="' + PATH + file + '">' + file + '</a></li>')
+    return '\n'.join(ret)
